38a170c63b73fa92199c79590bfa7d03922feaef
M Huzaifa Butt <zafibutt459@gmail.com>
2025-09-04 20:07:51 +0000

    Comparison of dataTypes

---
+++ javascriptclasses/04_comparisonofDtypes.js
@@ -0,0 +1,28 @@
+let x=2
+let y=3
+console.log(`Answer:${x>y}`)
+console.log(x<y)
+console.log(x>=y)
+console.log(x==y);
+console.log(x<=y)
+console.log(x===y)
+
+//Note : we can avoid the comparison of Datatype of these types bcz sometime it can behave differently and can't give the desired output and also code looking bad 
+
+// Datatype comparison
+
+console.log("2">1)
+console.log("3"< 2)
+console.log(null>0)
+console.log(null<9)
+console.log(null >=0)   //In this behave differently
+
+console.log(undefined>0)
+console.log(undefined<0)
+console.log(undefined>=0)
+console.log(undefined<=0)
+
+// ===
+console.log(1==="1")
+console.log(1===true)
+console.log("1"===true)
