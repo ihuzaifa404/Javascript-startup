fdbb86a1818ac55355f2f2df4976195b6072bdcd
M Huzaifa Butt <zafibutt459@gmail.com>
2025-09-04 14:39:18 +0000

    dataTypes

---
+++ javascriptclasses/02_dataTypes.js
@@ -0,0 +1,10 @@
+"use strict"
+let id=1
+let Name ="Ali"
+let Address
+let Pass =true
+let fail=null
+console.log([id, Name, Address, Pass, fail]);
+console.table([id, Name, Address, Pass, fail])
+console.log(typeof Name)
+console.table([typeof id, typeof Name, typeof Address, typeof Pass, typeof fail ])
